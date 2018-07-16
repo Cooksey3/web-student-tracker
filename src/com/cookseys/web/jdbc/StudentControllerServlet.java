ed6780c91cbbe24fccd7560f6e017d9d54046bee
Chris <chris@cookseys.org>
2018-07-16 10:31:58 -0400

    First commit

---
+++ src/com/cookseys/web/jdbc/StudentControllerServlet.java
@@ -0,0 +1,90 @@
+package com.cookseys.web.jdbc;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+/**
+ * Servlet implementation class StudentControllerServlet
+ */
+@WebServlet("/StudentControllerServlet")
+public class StudentControllerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	private StudentDbUtil studentDbUtil;
+	
+	@Resource(name="jdbc/web_student_tracker")
+	private DataSource dataSource;
+	
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		
+		try {
+			studentDbUtil = new StudentDbUtil(dataSource);
+		}
+		catch (Exception exc) {
+			throw new ServletException(exc);
+		}
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		try {
+			
+			String theCommand = request.getParameter("command");
+			
+			if (theCommand == null) {
+				theCommand= "LIST";
+			}
+			
+			switch (theCommand) {
+
+				case "LIST":
+					listStudents(request, response);
+					break;
+				
+				case "ADD":
+					addStudent(request, response);
+					break;
+				
+				default:
+					listStudents(request, response);
+			}
+			
+			
+			listStudents(request, response);
+		} catch (Exception exc) {
+			throw new ServletException(exc);
+		}			
+		
+	}
+
+	private void addStudent(HttpServletRequest request, HttpServletResponse response) {
+		
+		
+		
+	}
+
+	private void listStudents(HttpServletRequest request, HttpServletResponse response) 
+		throws Exception {
+
+		List<Student> students = studentDbUtil.getStudents();
+		
+		// add students to the request
+		request.setAttribute("STUDENT_LIST", students);
+				
+		// send to JSP page (view)
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/list-students.jsp");
+		dispatcher.forward(request, response);
+	}
+
+}
